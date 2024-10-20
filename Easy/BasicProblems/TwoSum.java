721b025b4fb29bf4e4bf96df06f16d201b71715f
Kashif <ahmed.kashif1001@gmail.com>
2024-10-20 21:48:32 +0500

    two sum problem no 1

--- Easy/BasicProblems/TwoSum.java
+++ Easy/BasicProblems/TwoSum.java
@@ -1,5 +1,33 @@
 package Easy.BasicProblems;
 
 public class TwoSum {
-    
+
+    // Make the twoSum method static
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) { // j starts from i + 1
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        // If no solution is found
+        return new int[] {};
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {2, 7, 11, 15};
+        int target = 9;
+        
+        // Call the twoSum method directly since it's static
+        int[] result = twoSum(nums, target);
+
+        // Print the result
+        if (result.length > 0) {
+            System.out.println("Indices: " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("No solution found.");
+        }
+    }
 }
+
