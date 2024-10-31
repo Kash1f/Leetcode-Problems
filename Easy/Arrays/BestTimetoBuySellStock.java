8249ad9cde89db048402cb55642c93ef5d8bdfc2
Kashif <ahmed.kashif1001@gmail.com>
2024-10-31 16:46:05 +0500

    code fixes

--- Easy/Arrays/BestTimetoBuySellStock.java
+++ Easy/Arrays/BestTimetoBuySellStock.java
@@ -1,21 +1,19 @@
 package Easy.Arrays;
 
-public class BestTimetoBuySellStock {
+class BestTimetoBuySellStock {
 
-public static int maxProfit(int[] prices){
+    public int maxProfit(int[] prices) {
         int minPrice = Integer.MAX_VALUE;
         int maxProfit = 0;
 
-        for(int i = 0; i < prices.length; i++){
-            if(prices[i] < minPrice){
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
                 minPrice = prices[i];
-            } else if (prices[i] - minPrice > maxProfit){
+            } else if (prices[i] - minPrice > maxProfit) {
                 maxProfit = prices[i] - minPrice;
             }
         }
-}
 
-    public static void main(String[] args) {
-        
+        return maxProfit;
     }
 }
---
+++ Easy/BasicProblems/Solution.java
@@ -0,0 +1,16 @@
+package Easy.BasicProblems;
+
+public class Solution Extends FirstBadVersion {
+    public int firstBadVersion(){
+        int left = 0;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+    }
+}
