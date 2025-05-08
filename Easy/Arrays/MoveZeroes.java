0dbebee71812bde36dd1a240511266e6bd17dcf6
Kashif <ahmed.kashif1001@gmail.com>
2025-05-09 02:14:08 +0500

    file changes

---
+++ Easy/Arrays/MoveZeroes.java
@@ -0,0 +1,10 @@
+package Easy.Arrays;
+
+class MoveZeroes {
+    public static int[] moveZeroes(int[] nums) {
+        
+    }
+}
+
+
+//we will traverse through each element and whenever we find a non-zero element, we will move it to the front of the array.
